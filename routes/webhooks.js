09eae55d65a4e479c1fb05dada7ec35b743c40e3
MadodaM <madoda@seabe.co.za>
2026-03-10 13:36:40 +0200

    fix: correct relative path to db service in routes

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const router = express.Router();
 //const { PrismaClient } = require('@prisma/client');
-const prisma = require('./services/db');
+const prisma = require('../services/db');
 //const prisma = new PrismaClient();
 const axios = require('axios');
 
