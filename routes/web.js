97869edf8efcaae3a862d6fb20c632798380bf64
Madoda <madoda@seabe.co.za>
2026-03-19 15:36:40 +0200

    fix: updated the cloudinary variables registration

--- routes/web.js
+++ routes/web.js
@@ -19,9 +19,9 @@ if (process.env.SENDGRID_KEY) sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 // --- 1. CLOUDINARY CONFIGURATION ---
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
 });
 
 // --- 2. STORAGE ENGINE ---
